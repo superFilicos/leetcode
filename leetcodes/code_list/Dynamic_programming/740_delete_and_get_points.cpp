3179934d8789947103c4ec5f4f6c01e692fcf3e2
xie <1454019631@qq.com>
2024-07-11 01:22:22 +0800

    first commit

---
+++ leetcodes/code_list/Dynamic_programming/740_delete_and_get_points.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+// class Solution {
+// public:
+//     int deleteAndEarn(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         for (auto i : nums){
+//             cout<< i ;
+//         }
+//         return 0;
+//     }
+// };
+
+int main(){
+    printf("helloworld");
+    cout<<"123";
+    // Solution s;
+    // vector<int> input = {1,2,3,4};
+    // s.deleteAndEarn(input);
+}
